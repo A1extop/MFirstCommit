00205d382336be9e13aaec7d2ece87d1cc7faca7
A1extop <blednykh1995@inbox.ru>
2024-05-14 16:52:33 +0300

    тестовый коммит

--- math/math.go
+++ math/math.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	a := "str"
-	b := "str"
-	fmt.Println(Max(a, b))
+	var a int = 5
+	var b int = 10
+	fmt.Println(Maximum(a, b))
+	fmt.Println(Minimum(a, b))
 }
 
-func Max[T comparable](one T, two T) bool {
-	return (one != two)
+func Maximum[T int | int32 | int64 | float32 | float64 | uint16 | uint32 | uint64](one, two T) T {
+	if one > two {
+		return one
+	}
+	return two
+}
+
+func Minimum[T int | int32 | int64 | float32 | float64 | uint16 | uint32 | uint64](one, two T) T {
+	if one < two {
+		return one
+	}
+	return two
 }
